68f00ba8830036b2520471652024dca1f3db01cd
umoriha <kxhoumu9x1231@hotmail.co.jp>
2021-06-09 01:38:48 +0900

    Initial Commit

---
+++ UmoriNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UmoriNews
+//
+//  Created by 片山紘 on 2021/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UmoriNews/UmoriNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmoriNewsApp.swift
+//  UmoriNews
+//
+//  Created by 片山紘 on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct UmoriNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
